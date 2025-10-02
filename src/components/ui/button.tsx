139cf4769a213a1bad3cebf9c26755e850f332d6
SelimC06 <scoskunuzer@gmail.com>
2025-10-02 12:16:13 -0500

    Revert "Switched from tauri to electron"
    This reverts commit 273addb55ee891361a4dc5ddbb6c2fde6ada667a.

--- src/components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-const DarkRoundedButton = () => {
-  return (
-    <button className="bg-zinc-900 border border-zinc-900 rounded-full inline-flex items-center justify-center py-3 px-7 text-center text-base font-medium text-white hover:bg-zinc-800 hover:border-zinc-800 disabled:bg-gray-300 disabled:border-gray-300 disabled:text-gray-500">
-      Get Started
-    </button>
-  )
-}
-
-export default DarkRoundedButton
